62a7acf926bce9d4c6bff84aab5cbe7f1302b96a
Pierre Brito <pierre.brito@example.com>
2026-05-30 01:23:47 -0300

    feat: move frontend assets to static folders

--- config/settings.py
+++ config/settings.py
@@ -71,6 +71,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 LOGIN_URL = "/"
 LOGIN_REDIRECT_URL = "/home/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
